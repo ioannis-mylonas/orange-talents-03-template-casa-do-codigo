02e01f8d5c64ed36ef91b2ab1cdd83966a87b756
ioannis-mylonas <ioannis.mylonas@gmail.com>
2021-04-07 18:22:18 -0300

    Melhorados testes validacao AutorForm

--- src/test/java/bootcamp/casacodigo/autor/form/AutorFormTest.java
+++ src/test/java/bootcamp/casacodigo/autor/form/AutorFormTest.java
@@ -9,12 +9,13 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.SpringConstraintValidatorFactory;
 
 import javax.validation.ConstraintViolation;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -29,58 +30,67 @@ class AutorFormTest {
     @Autowired
     private ConfigurableApplicationContext applicationContext;
 
-    private static Stream<Arguments> provideAutoresForValidacaoAutorVazioOuNull() {
-        return Stream.of(
-                Arguments.of(
-                        new AutorFormBuilder()
-                                .nome(null)
-                                .email(null)
-                                .descricao(null)
-                                .build(),
-                        false),
-                Arguments.of(
-                        new AutorFormBuilder()
-                                .nome(null)
-                                .email("um.email@email.com")
-                                .descricao("Uma descrição")
-                                .build(),
-                        false),
-                Arguments.of(
-                        new AutorFormBuilder()
-                                .nome("Autor Inválido")
-                                .email(null)
-                                .descricao("A descrição do autor...")
-                                .build(),
-                        false),
-                Arguments.of(
-                        new AutorFormBuilder()
-                                .nome("Autor Inválido")
-                                .email("email.valido@email.com")
-                                .descricao(null)
-                                .build(),
-                        false),
-                Arguments.of("", "", "", false),
-                Arguments.of("", "um.email@email.com", "Uma descrição", false),
-                Arguments.of("Meu Nome", "meu@email.com", "", false),
-                Arguments.of("Outro grande nome", "", "Minha descrição", false),
-                Arguments.of("João Joaquim", "joao.joaquim@email.com", "Ele se chama João", true),
-                Arguments.of("Joaquina de Assis Magalhães", "joaquina@lookout.com", "Ela se chama Joaquina", true)
-        );
+    private static Stream<Arguments> provideAutoresInvalidosForValidacaoAutorVazioOuNull() {
+        List<String> nomes = Arrays.asList(null, "", "       ");
+        List<String> emails = Arrays.asList(null, "", "       ", "emailinvalido");
+        List<String> descricoes = Arrays.asList(null, "", "      ");
+
+        Stream.Builder<Arguments> builder = Stream.builder();
+
+        for (String nome : nomes) {
+            for (String email : emails) {
+                for (String descricao : descricoes) {
+                    builder.add(Arguments.of(new AutorFormBuilder()
+                            .nome(nome)
+                            .email(email)
+                            .descricao(descricao)
+                            .build()));
+                }
+            }
+        }
+
+        return builder.build();
     }
 
-    private static Stream<Arguments> providenciaAutorparaValidacaoDuplicada() {
-        return Stream.of(
-                Arguments.of(
-                        new AutorFormBuilder()
-                                .nome("João Joaquim de Melos")
-                                .email("joao.joaquim@melos.com")
-                                .descricao("Autor Sr. Joaquim é formado.").build(),
-                        new AutorBuilder()
-                                .nome("João Joaquim de Melos")
-                                .email("joao.joaquim@melos.com")
-                                .descricao("Autor Sr. Joaquim é formado.").build(),
-                        false)
+    private static Stream<Arguments> providenciaAutorValidoParaValidacao() {
+        List<String> nomes = Arrays.asList("João Joaquim", "Aline Joanete", "Ellionel Leão");
+        List<String> emails = Arrays.asList("joao@joaquim.com", "aline@joanete.com", "ellionel.leao@email.com");
+        List<String> descricoes = Arrays.asList("Este é o Dr. João.", "Esta é a Dra. Aline.", "Este é o Prof. Ellionel.");
+
+        AutorFormBuilder builder = new AutorFormBuilder();
+        Stream.Builder<Arguments> streamBuilder = Stream.builder();
+
+        for (String nome : nomes) {
+            for (String email : emails) {
+                for (String descricao : descricoes) {
+                    streamBuilder.add(Arguments.of(
+                            builder.nome(nome).email(email).descricao(descricao).build()
+                    ));
+                }
+            }
+        }
+
+        return streamBuilder.build();
+    }
+
+    private static Stream<Arguments> providenciaAutorParaValidacaoDuplicada() {
+        List<AutorForm> autores = List.of(
+                new AutorForm("João Joaquim", "joao@joaquim.com", "Este é o Dr. João."),
+                new AutorForm("Aline Joanete", "aline@joanete.com", "Esta é a Dra. Aline."),
+                new AutorForm("Ellionel Leão", "ellionel.leao@email.com", "Este é o Prof. Ellionel.")
         );
+
+        Stream.Builder<Arguments> stream = Stream.builder();
+
+        for (AutorForm form : autores) {
+            for (AutorForm autor : autores) {
+                stream.add(Arguments.of(
+                        form, autor.converte(), form != autor
+                ));
+            }
+        }
+
+        return stream.build();
     }
 
     @BeforeEach
@@ -95,20 +105,25 @@ class AutorFormTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideAutoresForValidacaoAutorVazioOuNull")
-    public void validacaoAutorVazioOuNull(String nome, String email, String descricao, boolean esperado) {
-        AutorForm autor = new AutorForm(nome, email, descricao);
+    @MethodSource("provideAutoresInvalidosForValidacaoAutorVazioOuNull")
+    public void validacaoAutorVazioOuNull(AutorForm autor) {
         Set<ConstraintViolation<AutorForm>> errors = validator.validate(autor);
-
-        Assertions.assertEquals(esperado, errors.isEmpty());
+        Assertions.assertFalse(errors.isEmpty());
     }
 
     @ParameterizedTest
-    @MethodSource("providenciaAutorparaValidacaoDuplicada")
-    public void validacaoAutorJaExistente(AutorForm autor, Autor existente, boolean esperado) {
+    @MethodSource("providenciaAutorValidoParaValidacao")
+    public void validacaoAutorValido(AutorForm autor) {
+        Set<ConstraintViolation<AutorForm>> errors = validator.validate(autor);
+        Assertions.assertTrue(errors.isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("providenciaAutorParaValidacaoDuplicada")
+    public void validacaoAutorJaExistente(AutorForm autor, Autor existente, boolean valido) {
         autorRepository.save(existente);
         Set<ConstraintViolation<AutorForm>> errors = validator.validate(autor);
 
-        Assertions.assertEquals(esperado, errors.isEmpty());
+        Assertions.assertEquals(valido, errors.isEmpty());
     }
 }
